30f603ba2b480329c6c2419db0f5f67491491492
AqsalYS <aqsalyazids@gmail.com>
2026-07-24 21:56:46 +0700

    fix loading component

--- src/app/TambahMenu/page.js
+++ src/app/TambahMenu/page.js
@@ -1,10 +1,6 @@
 "use client"
 
-import { ArrowLeft, Check } from "lucide-react"
-import Loadingpage from "../../../component/OrderLoading"
-import Loadingskeleton from "../../../component/LoadingPage"
-import { useEffect, useRef, useState } from "react"
-import Modal from "../../../component/HistoryModal"
+import { useEffect, useState } from "react"
 
 const api = process.env.NEXT_PUBLIC_BASE_API;
 const coba = () => {
--- src/app/coba/page.js
+++ src/app/coba/page.js
@@ -1,12 +1,5 @@
 "use client"
 
-import { ArrowLeft, Check } from "lucide-react"
-import Loadingpage from "../../../component/OrderLoading"
-import Loadingskeleton from "../../../component/LoadingPage"
-import { useEffect, useRef, useState } from "react"
-import Modal from "../../../component/HistoryModal"
-import Image from "next/image"
-
 const api = process.env.NEXT_PUBLIC_BASE_API;
 const coba = () => {
   return(
